f39d2b7ba181f2df2545403439c5fbf9d5e4ec60
Fabio <fabiogodoy955@icloud.com>
2023-02-02 17:45:16 -0300

    feat: add global styles

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+    border: none;
+}
+
+:root{
+    --color-primary: #2EAFB2;
+}
+`;
+
+// font-family: 'Advent Pro', sans-serif;
+// font-family: 'Roboto', sans-serif;
