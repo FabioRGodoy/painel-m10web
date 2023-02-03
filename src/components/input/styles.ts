cdd920f435476f7d618c6a2565cdef60f42840d7
Fabio <fabiogodoy955@icloud.com>
2023-02-03 11:31:12 -0300

    feat: adding reusable input component

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  label {
+    margin-top: 15px;
+  }
+  .flexContainer {
+    position: relative;
+
+    .invisibleEye {
+      position: absolute;
+      right: 1%;
+      top: 35%;
+    }
+  }
+`;
 export const StyledInput = styled.input`
-  color: red;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: var(--border-radius);
+  height: 44px;
+  width: 100%;
+  padding-left: 10px;
 `;
