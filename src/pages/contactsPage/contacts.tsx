a7cda2620af5568d8ed2629f6b6b5b607226156f
Fabio <fabiogodoy955@icloud.com>
2023-02-04 00:18:51 -0300

    feat: add page contacts structure

---
+++ src/pages/contactsPage/contacts.tsx
@@ -0,0 +1,10 @@
+import { MobileNavBar } from "../../components/mobileNavBar/mobileNavBar";
+import { ContactsContainer } from "./styles";
+
+export const ContactPage = () => {
+  return (
+    <ContactsContainer>
+      <MobileNavBar />
+    </ContactsContainer>
+  );
+};
---
+++ src/pages/contactsPage/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ContactsContainer = styled.main`
+  background-color: var(--color-bg-home);
+  min-height: 100vh;
+`;
