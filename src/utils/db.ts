6b777f45cb053fd819d595dda94f5337a4c2bff3
Fabio <fabiogodoy955@icloud.com>
2023-02-04 00:19:52 -0300

    feat: add fake database

---
+++ src/utils/db.ts
@@ -0,0 +1,72 @@
+export const database = [
+  {
+    id: 1,
+    imageUrl: "",
+    title: "What is Lorem Ipsum?",
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.",
+  },
+  {
+    id: 2,
+    imageUrl: "",
+    title: "What is Lorem Ipsum?",
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.",
+  },
+  {
+    id: 3,
+    imageUrl: "",
+    title: "What is Lorem Ipsum?",
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.",
+  },
+  {
+    id: 4,
+    imageUrl: "",
+    title: "What is Lorem Ipsum?",
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.",
+  },
+  {
+    id: 5,
+    imageUrl: "",
+    title: "What is Lorem Ipsum?",
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.",
+  },
+  {
+    id: 7,
+    imageUrl: "",
+    title: "What is Lorem Ipsum?",
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.",
+  },
+  {
+    id: 8,
+    imageUrl: "",
+    title: "What is Lorem Ipsum?",
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.",
+  },
+  {
+    id: 9,
+    imageUrl: "",
+    title: "What is Lorem Ipsum?",
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.",
+  },
+  {
+    id: 10,
+    imageUrl: "",
+    title: "What is Lorem Ipsum?",
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.",
+  },
+  {
+    id: 11,
+    imageUrl: "",
+    title: "What is Lorem Ipsum?",
+    description:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.",
+  },
+];
