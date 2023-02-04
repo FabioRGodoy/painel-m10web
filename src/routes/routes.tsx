5907c7531473c08b62074eacf26f6470439f7d74
Fabio <fabiogodoy955@icloud.com>
2023-02-04 00:19:24 -0300

    feat: add home page route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "../pages/loginPage/login";
 import { HomePage } from "../pages/homePage/home";
+import { ContactPage } from "../pages/contactsPage/contacts";
 
 export const AllRoutes = () => {
   return (
     <Routes>
       <Route index element={<LoginPage />} />
-      <Route path="/home" element={<HomePage />} />
+      <Route path="home/:page" element={<HomePage />} />
+      <Route path="tropa/:page" element={<ContactPage />} />
     </Routes>
   );
 };
