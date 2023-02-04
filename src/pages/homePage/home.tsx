d4094d33f6b23e10515234cd3a49559bba880319
Fabio <fabiogodoy955@icloud.com>
2023-02-04 00:19:07 -0300

    feat: add home page

--- src/pages/homePage/home.tsx
+++ src/pages/homePage/home.tsx
@@ -1,7 +1,41 @@
+import { NavBar } from "../../components/navigationBar/navBar";
+import { ContentContainer, HomeContainer } from "./styles";
+import { database } from "../../utils/db";
+import { CardComponent } from "../../components/card/card";
+import { useParams } from "react-router-dom";
+
 export const HomePage = () => {
+  const { page } = useParams();
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <>
+      {page === "dashboard" ? (
+        <HomeContainer>
+          <NavBar />
+          <ContentContainer>
+            <section>
+              <h1>
+                Olá <span>Usuário</span>
+              </h1>
+              <div className="line"></div>
+              <section>
+                <ul>
+                  {database.map((item) => {
+                    return (
+                      <CardComponent
+                        key={item.id}
+                        title={item.title}
+                        description={item.description}
+                      />
+                    );
+                  })}
+                </ul>
+              </section>
+            </section>
+          </ContentContainer>
+        </HomeContainer>
+      ) : (
+        <h1>404 NOT FOUND</h1>
+      )}
+    </>
   );
 };
--- src/pages/homePage/styles.ts
+++ src/pages/homePage/styles.ts
@@ -1,1 +1,37 @@
 import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+  background-color: var(--color-bg-home);
+  min-height: 100vh;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ContentContainer = styled.div`
+  width: 90%;
+  padding: 40px;
+  margin-left: 220px;
+
+  h1 {
+    margin-bottom: 20px;
+    font-size: 2rem;
+    color: var(--color-text-grey);
+    font-weight: 400;
+
+    span {
+      font-weight: 700;
+    }
+  }
+  .line {
+    width: 100%;
+    height: 1px;
+    border: 1px solid #00000010;
+  }
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    margin-top: 20px;
+  }
+`;
